0631fbc2b44edc07ae6b54e4493b10067842ef22
axel <axel.aatl@gmail.com>
2021-05-13 17:24:44 -0400

    Create app and UI Design

---
+++ app/src/main/java/com/example/geckoapplication/ui/main/PlaceholderFragment.kt
@@ -0,0 +1,24 @@
+package com.example.geckoapplication.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.geckoapplication.R
+
+class PlaceholderFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
